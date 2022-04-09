e87e224a94bfdd4f87860d070b274921d2cdcff1
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2022-04-08 21:51:16 -0400

    Refactoring
    Moved business logic out of UI (program.cs) into GameController.cs

--- RPSLS/GameController.cs
+++ RPSLS/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RPSLS
 {
@@ -8,12 +9,14 @@ namespace RPSLS
         public int MaxTurns { get; set; } = 3;
         public int playerScore = 0;
         public int computerScore = 0;
-        public List<Move> computerMoves = new List<Move>();
-        public List<Move> moveDatabase = new List<Move>();
+        public int currentTurn = 0;
+        public string gameWinner = "";
+        public List<Move> computerMoves = new();
+        public List<Move> moveDatabase = new();
         private Move lastMove = null;
 
         public GameController(int maxTurns)
-        { 
+        {
             if (maxTurns < 3)
             {
                 MaxTurns = 3;
@@ -72,7 +75,7 @@ namespace RPSLS
             moveDatabase.Add(lizard);
             moveDatabase.Add(spock);
         }
-      
+
         public Move GetComputerMove()
         {
             var selectRandomMove = new Random();
@@ -91,5 +94,122 @@ namespace RPSLS
             return move;
         }
 
+        public void PlayGame()
+        {
+            currentTurn = 1;
+
+            //Begin Gameplay
+            while (currentTurn <= this.MaxTurns)
+            {
+                Console.WriteLine($"Round {currentTurn}");
+                var computerChoice = GetComputerMove();
+
+                var playerChoice = GetPlayerMove();
+
+                Console.WriteLine($"You chose: {playerChoice.Name}");
+
+                System.Threading.Thread.Sleep(300);
+
+                Console.WriteLine($"Computer Chose: {computerChoice.Name}");
+
+                CheckMove(playerChoice, computerChoice);
+
+            }
+
+            EndGame();
+
+        }
+
+        public Move GetPlayerMove()
+        {
+            var goodChoice = false;
+            IEnumerable<Move> moveChoice = null;
+            Move playerMove = null;
+
+            while (goodChoice == false)
+            {
+                Console.WriteLine("(rock, paper, scissors, lizard, spock)");
+                Console.WriteLine("Choice:");
+
+                try
+                {
+                    var choice = Console.ReadLine();
+                    moveChoice = from move in moveDatabase
+                                 where move.Name.ToLower() == choice.ToLower()
+                                 select move;
+                    if (moveChoice.Any())
+                    {
+                        goodChoice = true;
+                        playerMove = moveChoice.First();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Move name not found. Please try again.");
+                        continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Move name not found. Please try again.");
+                    continue;
+                }
+            }
+            return playerMove;
+
+        }
+
+        public void CheckMove(Move playerChoice, Move computerChoice)
+        {
+            if (playerChoice.Name == computerChoice.Name)
+            {
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine($"Draw! {playerChoice.Name} is the same as {computerChoice.Name}");
+                currentTurn++;
+                Console.WriteLine("");
+            }
+
+            if (playerChoice.WeakTo(computerChoice))
+            {
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine($"You lose this round! {computerChoice.Name} beats {playerChoice.Name}");
+                computerScore += 1;
+                currentTurn++;
+                Console.WriteLine("");
+            }
+            else if (computerChoice.WeakTo(playerChoice))
+            {
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine($"You win this round! {playerChoice.Name} beats {computerChoice.Name}");
+                playerScore += 1;
+                currentTurn++;
+                Console.WriteLine("");
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine($"Uncaught exception! {playerChoice.Name} versus {computerChoice.Name}");
+                Console.WriteLine("");
+            }
+        }
+
+        public void EndGame()
+        {
+            Console.WriteLine("END OF GAME RESULTS");
+
+            Console.WriteLine($"Player Score: {playerScore} | Computer Score: {computerScore}");
+            if (playerScore == computerScore)
+            {
+                Console.WriteLine("Its a draw!");
+            }
+            if (playerScore > computerScore)
+            {
+                Console.WriteLine("You win!");
+            }
+            if (playerScore < computerScore)
+            {
+                Console.WriteLine("You lose!");
+            }
+        }
     }
 }
--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace RPSLS
 {
@@ -39,93 +38,10 @@ namespace RPSLS
 
             }
 
-            GameController gc = new GameController(maxTurns);
+            GameController gc = new(maxTurns);
 
-            var currentTurn = 1;
+            gc.PlayGame();
 
-            //Begin Gameplay
-            while (currentTurn <= maxTurns)
-            {
-                Console.WriteLine($"Round {currentTurn} - Please enter your move choice.");
-                Console.WriteLine("(rock, paper, scissors, lizard, spock)");
-                Console.WriteLine("Choice:");
-                var choice = Console.ReadLine();
-                try
-                {
-                    var moveChoice = from move in gc.moveDatabase
-                                     where move.Name.ToLower() == choice.ToLower()
-                                     select move;
-                    var computerChoice = gc.GetComputerMove();
-
-                    var playerChoice = moveChoice.FirstOrDefault();
-
-                    Console.WriteLine($"You chose: {playerChoice.Name}");
-                    System.Threading.Thread.Sleep(300);
-                    Console.WriteLine($"Computer Chose: {computerChoice.Name}");
-
-                    if (playerChoice.Name == computerChoice.Name)
-                    {
-                        System.Threading.Thread.Sleep(300);
-                        Console.WriteLine($"Draw! {playerChoice.Name} is the same as {computerChoice.Name}");
-                        currentTurn++;
-                        Console.WriteLine("");
-                        continue;
-                    }
-
-                    if (playerChoice.WeakTo(computerChoice))
-                    {
-                        System.Threading.Thread.Sleep(300);
-                        Console.WriteLine($"You lose this round! {computerChoice.Name} beats {playerChoice.Name}");
-                        gc.computerScore += 1;
-                        currentTurn++;
-                        Console.WriteLine("");
-                        continue;
-                    }
-                    else if (computerChoice.WeakTo(playerChoice))
-                    {
-                        System.Threading.Thread.Sleep(300);
-                        Console.WriteLine($"You win this round! {playerChoice.Name} beats {computerChoice.Name}");
-                        gc.playerScore += 1;
-                        currentTurn++;
-                        Console.WriteLine("");
-                        continue;
-                    }
-                    else
-                    {
-                        System.Threading.Thread.Sleep(300);
-                        Console.WriteLine($"Uncaught exception! {playerChoice.Name} versus {computerChoice.Name}");
-                        Console.WriteLine("");
-                        continue;
-                    }
-                    {
-
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    Console.WriteLine("Move name not found. Please try again.");
-                    continue;
-                }
-            }
-
-            System.Threading.Thread.Sleep(300);
-            Console.WriteLine("END OF GAME RESULTS");
-
-
-            Console.WriteLine($"Player Score: {gc.playerScore} | Computer Score: {gc.computerScore}");
-            if (gc.playerScore == gc.computerScore)
-            {
-                Console.WriteLine("Its a draw!");
-            }
-            if (gc.playerScore > gc.computerScore)
-            {
-                Console.WriteLine("You win!");
-            }
-            if (gc.playerScore < gc.computerScore)
-            {
-                Console.WriteLine("You lose!");
-            }
         }
     }
 }
